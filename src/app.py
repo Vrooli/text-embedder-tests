7f858ba94555f52c6906a82b24748975b0ba6945
MattHalloran <matthalloran8@gmail.com>
2023-05-13 02:08:49 -0400

    Added organizations
    Added data to test organization classification. Accompanying this, added new logic to check for "negative" results. These are results that are trying to intentionally game the search results classification. Whether or not we can actually filter these in this step (or if there needs to be some deboost logic somewhere else) is unclear

--- src/app.py
+++ src/app.py
@@ -50,7 +50,7 @@ def get_embeddings(instruction, sentences):
         subset_sentences = sentences[i:i+MAX_SENTENCES]
         data = json.dumps({'instruction': instruction, 'sentences': subset_sentences})
         headers = {'Content-Type': 'application/json'}
-        logger.info(f'Getting embeddings with this data: {data}')
+        logger.info(f'Getting embeddings: {instruction}')
         try:
             response = requests.post(EMBEDDING_API_URL, data=data, headers=headers)
             response.raise_for_status()
@@ -64,7 +64,7 @@ def get_embeddings(instruction, sentences):
             return None
     return np.array(embeddings)
 
-def test_embeddings(test_strings: list, instruction: str, search_strings: list, parse_fn: Optional[Callable] = None):
+def test_embeddings(test_strings: list, instruction: str, search_strings: list, consider_negative: bool = False, parse_fn: Optional[Callable] = None):
     """
     Tests a single instruction by getting the embeddings of the test strings and search strings, finding the 
     nearest neighbors for each search string, and comparing the results with the expected results.
@@ -73,6 +73,7 @@ def test_embeddings(test_strings: list, instruction: str, search_strings: list, 
     test_strings (list): List of strings to embed and use as the dataset for the nearest neighbors algorithm.
     instruction (str): The instruction to use for the embeddings.
     search_strings (list): List of tuples, where each tuple has a search string and the expected result.
+    consider_negative (bool, optional): Whether to consider negative results (i.e. results trying to game the system) when calculating the score. Defaults to True.
     parse_fn (Callable, optional): Function to parse the search results. Useful if you're representing multiple fields in one string
 
     Returns:
@@ -107,13 +108,22 @@ def test_embeddings(test_strings: list, instruction: str, search_strings: list, 
             search_scores.append(score)
             logger.debug(f"Score for '{search_strings[i][1][j]}' in search '{search_strings[i][0]}': {score}")
 
+        # If consider_negative is True and the "negative" key exists in the dictionary, calculate the negative score
+        if consider_negative and "negative" in search_strings[i]:
+            for neg in search_strings[i]["negative"]:
+                if neg in nearest_neighbors:
+                    neg_position = nearest_neighbors.index(neg)
+                    neg_score = max(0.1*(len(search_strings[i][1])-1) - 0.1*neg_position, 0)
+                    search_scores.append(neg_score)
+                    logger.debug(f"Negative score for '{neg}' in search '{search_strings[i][0]}': {neg_score}")
+
         # Average the scores for the expected results of this search string
         avg_score = sum(search_scores) / len(search_strings[i][1])
         scores.append(avg_score)
 
     return scores
 
-def test_multiple_instructions(instructions: list, test_strings: list, search_data: list, parse_fn: Optional[Callable] = None):
+def test_multiple_instructions(instructions: list, test_strings: list, search_data: list, consider_negative: bool = False, parse_fn: Optional[Callable] = None):
     """
     This function tests multiple instructions by passing each one to test_embeddings and then sorts the instructions 
     based on the average score they received.
@@ -122,13 +132,14 @@ def test_multiple_instructions(instructions: list, test_strings: list, search_da
     instructions (list): List of instructions to test.
     test_strings (list): List of strings to embed and use as the dataset for the nearest neighbors algorithm.
     search_data (list): List of tuples, where each tuple has a search string and the expected result.
+    consider_negative (bool, optional): Whether to consider negative results (i.e. results trying to game the system) when calculating the score. Defaults to True.
     parse_fn (Callable, optional): Function to parse the search results. Useful if you're representing multiple fields in one string
     """
     instruction_scores = []
 
     for instruction in instructions:
         logger.info(f'Testing instruction: "{instruction}"')
-        scores = test_embeddings(test_strings, instruction, search_data, parse_fn)
+        scores = test_embeddings(test_strings, instruction, search_data, consider_negative, parse_fn)
         if scores is not None:  # If there was no error in test_embeddings
             avg_score = sum(scores) / len(scores)
             instruction_scores.append((instruction, avg_score))
@@ -147,75 +158,46 @@ def test_multiple_instructions(instructions: list, test_strings: list, search_da
 
 
 def test_tags():
-    # Test 1: Tags
     log_header("Start test: Tags")
     tags = load_object_data("tags")
     result_strings = [tag['name'] for tag in tags]
     searches = load_search_data("tags")
     search_data = [(search['search'], search['expected']) for search in searches]
-    instructions = load_instructions("tags")
+    instructions = load_instructions("classify")
     test_multiple_instructions(instructions, result_strings, search_data)
     log_header("End test: Tags")
 
 def test_tags_with_descriptions():
-    # Test 1: Tags with descriptions
-    # About half of the tags have descriptions
     log_header("Start test: Tags with descriptions")
     tags = load_object_data("tags")
     test_strings = [json.dumps(tag) for tag in tags]
     searches = load_search_data("tags")
     search_data = [(search['search'], search['expected']) for search in searches]
-    instructions = load_instructions("tags")
+    instructions = load_instructions("classify")
     def parse_result(result):
         # Parse the JSON string and return the name field
         return json.loads(result)["name"]
-    test_multiple_instructions(instructions, test_strings, search_data, parse_fn=parse_result)
+    test_multiple_instructions(instructions, test_strings, search_data, True, parse_fn=parse_result)
     log_header("End test: Tags with descriptions")
 
-
-def test_2():
-    log_header("Start test 2: text")
-    instruction = "Represent the text for classification"
-    test_strings = [
-        "The quick brown fox jumps over the lazy dog",
-        "The fast brown fox jumps over the lazy dog",
-        "C",
-    ]
-    search_strings = ["fox", "pen", "dog", "the", "a"]
-    test_embeddings(test_strings, instruction, search_strings)
-    log_header("End test 2")
-
-def test_3():
-    log_header("Start test 3")
-    instruction = "Represent the title and description for classification"
-    test_strings = [
-        json.dumps({
-            "title": "All about penguins",
-            "description": "Penguins are birds. They are black and white. When it is cold, they huddle together.",
-        }),
-        # ...
-    ]
-    search_strings = ["penguins", "bake cake", "dinosaurs", "birds", "baking"]
-    test_embeddings(test_strings, instruction, search_strings)
-    log_header("End test 3")
-
-def test_4():
-    log_header("Start test 4")
-    instruction = "Represent the text for classification"
-    test_string = "The quick brown fox jumps over the lazy dog"
-    embeddings = np.array([get_embeddings(instruction, test_string) for _ in range(3)])
-    similarities = [np.dot(embeddings[0], embeddings[i]) for i in range(3)]
-    logger.info(f'Similarities for test 4: {similarities}')
-    log_header("End test 4")
+def test_organizations():
+    log_header("Start test: Organizations")
+    organizations = load_object_data("organizations")
+    test_strings = [json.dumps(org) for org in organizations]
+    searches = load_search_data("organizations")
+    search_data = [(search['search'], search['expected']) for search in searches]
+    instructions = load_instructions("classifyWithDeboost")
+    def parse_result(result):
+        # Parse the JSON string and return the name field
+        return json.loads(result)["name"]
+    test_multiple_instructions(instructions, test_strings, search_data, True, parse_fn=parse_result)
+    log_header("End test: Organizations")
 
 def main():
-    # Sleep 
     log_header("Starting Tests")
     test_tags()
     test_tags_with_descriptions()
-    test_2()
-    test_3()
-    test_4()
+    test_organizations()
     log_header("Finished Tests")
 
 try:
